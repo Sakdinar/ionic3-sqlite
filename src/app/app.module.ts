8dd8770aeb698acb93069885f27e3692d0ad653d
Felipe Del Valle - FVL <felipe.valle@iteris.local>
2017-11-29 10:36:44 -0200

    added func create, insert, select, drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { SQLite } from '@ionic-native/sqlite';
+import { Toast } from '@ionic-native/toast';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +26,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    SQLite,
+    Toast,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
